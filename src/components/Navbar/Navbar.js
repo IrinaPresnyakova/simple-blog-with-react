2c8fb626b7347fb0324c0221573131e0892a3961
Irina Presnyakova <ipresnyakova@gmail.com>
2022-05-05 11:20:05 -0700

    Basic layout with two components and a button

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.scss'
+// import '../../index.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Irina's Blog</h1> 
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
